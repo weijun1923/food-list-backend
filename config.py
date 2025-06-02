e4046b78565f53ce987f94fc97a5eaef8e6f4334
root <root@DESKTOP-POTSRK8.>
2025-06-02 16:50:25 +0800

    feat(database): add the supabase url connect

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
 from datetime import timedelta
+from typing import Optional
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:ads101250101@db.akaotykfvgucqeafbffv.supabase.co:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "jwt-secret-key"
     JWT_VERIFY_SUB = False
